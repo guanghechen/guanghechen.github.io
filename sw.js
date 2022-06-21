59651c4b828921d52a41a1d8afcdb010d84e713b
guanghechen <42513619+guanghechen@users.noreply.github.com>
2022-06-21 14:19:24 +0000

    Deploying to blog from @ guanghechen/blog@b52450ebc6d3f099f1787fe3287e0c6196dc6bba 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6f84da2ac3aa651dd1c9.js"
+    "url": "webpack-runtime-91e8a0284a829c987f33.js"
   },
   {
     "url": "styles.a4bcfeff4f3b823a108b.css"
@@ -42,11 +42,11 @@ self.__precacheManifest = [
     "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-a28d0d66dde68c8edab4.js"
   },
   {
-    "url": "app-17814c4e567098aea5b4.js"
+    "url": "app-c9750bb43aff08598a8b.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7ad91fbe7ac9f5c9a42d6da0b5b94dba"
+    "revision": "444a4d994abe400d82c761ba5e766074"
   },
   {
     "url": "polyfill-45a28b92b55139ab2301.js"
@@ -163,7 +163,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-17814c4e567098aea5b4.js`))) {
+  if (!resources || !(await caches.match(`/app-c9750bb43aff08598a8b.js`))) {
     return await fetch(event.request)
   }
 
